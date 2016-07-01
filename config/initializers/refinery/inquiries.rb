d04925d8a15f19db5a618e2738b0c2d92e921abf
flyptkarsh <ptkarsh@gmail.com>
2016-06-30 20:14:57 -0400

    added inquires

--- config/initializers/refinery/inquiries.rb
+++ config/initializers/refinery/inquiries.rb
@@ -3,10 +3,10 @@ Refinery::Inquiries.configure do |config|
   # config.show_contact_privacy_link = true
 
   # Configure whether to show phone number field
-  config.show_phone_number_field = true
+  # config.show_phone_number_field = true
 
   # Configure whether to show form field placeholders
-  config.show_placeholders = true
+  # config.show_placeholders = true
 
   # Configure whether inquiries marked as spam should also send a notification mail
   # config.send_notifications_for_inquiries_marked_as_spam = false
@@ -15,9 +15,9 @@ Refinery::Inquiries.configure do |config|
   # config.from_name = "no-reply"
 
   # Configure the URLs to use for the inquiry functionality.
-  config.post_path           = "/guest_book"
-  config.page_path_new       = "/guest_book"
-  config.page_path_thank_you = "/guest_book/thank_you"
+  # config.post_path           = "/contact"
+  # config.page_path_new       = "/contact"
+  # config.page_path_thank_you = "/contact/thank_you"
 
   # Toggle the spam filter
   # config.filter_spam = true
